656f488c0925d44863395c5a0b2491333870e53d
Ayon Bhowmick <ayb224@lehigh.edu>
2022-01-29 19:01:36 -0500

    added susfoods

--- web/src/App.js
+++ web/src/App.js
@@ -7,9 +7,9 @@ import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Box from '@mui/system/Box';
-import Home from './home/Home.jsx';
+import Home from './home/Home';
 import Contact from "./contact/Contact";
-
+import Projects from "./projects/Projects";
 
 const elements = {
 	EL1: {},
@@ -27,36 +27,61 @@ const App = () => {
 			<AnchorNav elements={elements} shouldEnableHistory={true} shouldModifyUrl={true}>
 				{({ refs, activeElement, goTo }) => (
 					<div>
-						<Fab sx={{zIndex:1, position:'fixed', marginLeft:"95%", marginBottom:"95%", top:"2%"}} onClick={() => setMenu(true)}>
+						<Fab sx={{zIndex:3, position:'fixed', marginLeft:"95%", marginBottom:"95%", top:"2%", color:"#945197"}} onClick={() => setMenu(true)}>
 							<MenuRoundedIcon/>
 						</Fab>
-						<Drawer open={menu} anchor="right" onClose={() => setMenu(false)}>
-							<Box sx={{width:250}}>
+						<Drawer sx={{color:"#191919"}} open={menu} anchor="right" onClose={() => setMenu(false)}>
+							<Box sx={{width:250, background:"#191919", height:"100%"}}>
 								<List>
-									<ListItem className={activeElement === "EL1" ? "selected-menuitem" : "menuitem"} onClick={() => {setMenu(false); goTo("EL1");}}>
+									{activeElement === "EL1" ? 
+									<ListItem sx={{color:"#945197", cursor: "pointer"}} onClick={() =>  {setMenu(false); goTo("EL1");}}>
 										<h3>Ayon Bhowmick</h3>
 									</ListItem>
-									<ListItem onClick={() => {setMenu(false); goTo("EL2");}}>
-										<h3 className={activeElement === "EL1" ? "selected-menuitem" : "menuitem"}>Projects</h3>
+									:
+									<ListItem sx={{color:"#F5F5F5", cursor: "pointer"}} onClick={() =>  {setMenu(false); goTo("EL1");}}>
+										<h3>Ayon Bhowmick</h3>
+									</ListItem>}
+									{activeElement === "EL2" ? 
+									<ListItem sx={{color:"#945197", cursor: "pointer"}} onClick={() =>  {setMenu(false); goTo("EL2");}}>
+										<h3>Projects</h3>
 									</ListItem>
-									<ListItem onClick={() => {setMenu(false); goTo("EL3");}}>
-										<h3 className={activeElement === "EL1" ? "selected-menuitem" : "menuitem"}>Skills</h3>
+									:
+									<ListItem sx={{color:"#F5F5F5", cursor: "pointer"}} onClick={() =>  {setMenu(false); goTo("EL2");}}>
+										<h3>Projects</h3>
+									</ListItem>}
+									{activeElement === "EL3" ? 
+									<ListItem sx={{color:"#945197", cursor: "pointer"}} onClick={() =>  {setMenu(false); goTo("EL3");}}>
+										<h3>Skills</h3>
 									</ListItem>
-									<ListItem onClick={() => {setMenu(false); goTo("EL4");}}>
-										<h3 className={activeElement === "EL1" ? "selected-menuitem" : "menuitem"}>Awards/Experience</h3>
+									:
+									<ListItem sx={{color:"#F5F5F5", cursor: "pointer"}} onClick={() =>  {setMenu(false); goTo("EL3");}}>
+										<h3>Skills</h3>
+									</ListItem>}
+									{activeElement === "EL4" ? 
+									<ListItem sx={{color:"#945197", cursor: "pointer"}} onClick={() =>  {setMenu(false); goTo("EL4");}}>
+										<h3>Awards/Experience</h3>
 									</ListItem>
-									<ListItem onClick={() => {setMenu(false); goTo("EL5");}}>
-										<h3 className="selected-menuitem">Contact</h3>
+									:
+									<ListItem sx={{color:"#F5F5F5", cursor: "pointer"}} onClick={() =>  {setMenu(false); goTo("EL4");}}>
+										<h3>Awards/Experience</h3>
+									</ListItem>}
+									{activeElement === "EL5" ? 
+									<ListItem sx={{color:"#945197", cursor: "pointer"}} onClick={() =>  {setMenu(false); goTo("EL5");}}>
+										<h3>Contact</h3>
 									</ListItem>
+									:
+									<ListItem sx={{color:"#F5F5F5", cursor: "pointer"	}} onClick={() =>  {setMenu(false); goTo("EL5");}}>
+										<h3>Contact</h3>
+									</ListItem>}
 								</List>
 							</Box>
 						</Drawer>
-						<div>
-							<div ref={refs.EL1} className="home">
+						<div className="stack">
+							<div ref={refs.EL1}>
 								<Home/>
 							</div>
-							<div ref={refs.EL2} className="content">
-								<h1>Projects</h1>
+							<div ref={refs.EL2}>
+								<Projects/>
 							</div>
 							<div ref={refs.EL3} className="content">
 								<h1>Skills</h1>
--- web/src/projects/Projects.jsx
+++ web/src/projects/Projects.jsx
@@ -1,7 +1,45 @@
+import "./projects.css";
+import { useInView } from "react-intersection-observer"
+import Button from '@mui/material/Button';
+
 const Projects = () => {
+    const [projTitle, setProjTitle] = useInView({triggerOnce: true, threshold: 0.5});
+
     return (
         <div className="projects">
-            <h1>Projects</h1>
+            <div className="invisible-top-projects"/>
+            <div className={setProjTitle ? "invisible-projects": null} ref={projTitle}/>
+            <div className={setProjTitle ? "head-projects" : null}>
+                <h2>Projects</h2>
+            </div>
+            <div className="projects-container">
+                <div className="invisible-left-projects"/>
+                <img src="SusLogo.png" alt="SusLogo"/>
+                <div className={setProjTitle ? "project-description animate" : "project-description"}>
+                    <h3>Sus Foods</h3>
+                    <p>
+                        Sus Foods is a web app that combats the problem of food waste. It does this by tracking the expiration dates of your perishables and reminds you when they are going to go bad. This project was made for the HackHolyoke 2021 hackathon and won.
+                    </p>
+                    <p>
+                        Made with:
+                    </p>
+                    <div className="made-with-list">
+                        <h5>JavaScript</h5>
+                        <h5>React.js</h5>
+                        <h5>Node.js</h5>
+                        <h5>Material UI</h5>
+                        <h5>CSS</h5>
+                    </div>
+                    <div className="project-links">
+                        <Button href="https://github.com/Ayon-Bhowmick/HackHolyoke" target="_blank">
+                            <img src="Octicons-mark-github.svg" alt="Github"/>
+                        </Button>
+                        <Button href="https://devpost.com/software/susfoods-for-a-sustainable-future" target="_blank">
+                            <img src="devpost.svg" alt="devPost"/>
+                        </Button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
